8ac373037295044f3aaeaaffa32c2d8b6fbd80d7
Mario <mario@pop-os.localdomain>
2021-03-10 00:17:52 -0700

    Warning color changed to purple

--- run_13/constants.py
+++ run_13/constants.py
@@ -10,6 +10,6 @@
 #WARNING, if cnn's stride is changed, paddings may result incorrect
 CNN_STRIDE = 2
 R = '\033[31m' #Used to print red messages in terminal
-O = '\033[33m' #Used to print orange messages in terminal
+P = '\033[35m' #Used to print purple messages in terminal
 W = '\033[0m'  #Used to print white messages in terminal (default)
 G = '\033[32m' #Used to print green messages in terminal (default)
