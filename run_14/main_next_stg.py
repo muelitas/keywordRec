4de563c7cca6c19c7442a0cb1927ec16939f7733
Mario <mario@pop-os.localdomain>
2021-03-16 12:18:39 -0600

    Added code for new scheduler; added KAx4 and TSx4

--- run_14/main_next_stg.py
+++ run_14/main_next_stg.py
@@ -10,7 +10,6 @@
 ***************************************************************************''' 
 import copy 
 import gc
-import math
 import os
 from os.path import join as pj #pj stands for path.join
 from pathlib import Path
@@ -24,19 +23,18 @@ import torch.optim as optim
 import warnings
 
 import constants as cnstnt
-from models import SpeechRecognitionModel, BiGRU
+from models import SpeechRecognitionModel
 from preprocessing import preprocess_data, check_folder, get_mappings
 from utils import data_processing, train, dev, Metrics, CUSTOM_DATASET, \
-    plot_and_save, log_message, find_best_lr, error, \
+    plot_and_save, log_message, error, LR_SCHED, \
     log_model_information, save_chckpnt, log_labels, log_k_words_instances, \
     update_cnn, update_classifier, update_bigru
 
 #Comment this from time to time and check warnings are the same
 warnings.filterwarnings("ignore")
-THIS NEEDS TO BE UPDATED, COMPARE IT WITH MAIN 1ST STAGE
+
 ##############################################################################
 #VARIABLES THAT MIGHT NEED TO BE CHANGED ARE ENCLOSED IN THESE HASHTAGS
-FIND_LR = False #find learning rate
 TRAIN = True #train and test!
 
 #Root location of logs, plots and checkpoints
@@ -44,10 +42,10 @@ runs_root = str(Path.home()) + '/Desktop/ctc_runs'
 #Root location of spectrograms and dictionaries
 data_root = str(Path.home()) + '/Desktop/ctc_data'
 
-prev_chckpt_dir = 'TS_50E' #Previous checkpoint folder name
+prev_chckpt_dir = 'ML2_yTSx4_60E3' #Previous checkpoint folder name
 prev_chckpt_stg = 'stg1' #Previous checkpoint stage name
-new_chckpt_stg = 'stg_dummy2' #Stage name for new checkpoint
-prev_chckpt_name = 'checkpoint_onRun01onEpoch047.tar'
+new_chckpt_stg = 'stg_dummy' #Stage name for new checkpoint
+prev_chckpt_name = 'checkpoint_onRun01onEpoch023.tar'
 new_chckpt_name = 'checkpoint.tar'
 
 #Paths to logs
@@ -81,7 +79,18 @@ TS_data = {
     'train_csv': gt_csvs_folder + '/ts_train.csv',
     'dev_csv': gt_csvs_folder + '/ts_dev.csv',
     'splits': [0.9, 0.1],
-    'num': 200 #Set equal to None if you want to use all audios
+    'num': 3000 #Set equal to None if you want to use all audios
+}
+#TTS and gTTS's dataset (run through pyroom, 4 diff. locations)
+TSx4 = {
+    'dataset_ID': 'TSx4',
+    'use_dataset': 0,
+    'dict': data_root + '/dict/ts_dict.pickle',
+    'transcript': data_root + '/spctrgrms/pyroom/TSx4/transcript.txt',
+    'train_csv': gt_csvs_folder + '/ts_x4_train.csv',
+    'dev_csv': gt_csvs_folder + '/ts_x4_dev.csv',
+    'splits': [0.9, 0.1],
+    'num': 3000 #Set equal to None if you want to use all audios
 }
 
 TS_kwords = {
@@ -114,6 +123,17 @@ KA_data = {
     'transcript': data_root + '/spctrgrms/clean/KA/transcript.txt',
     'train_csv': gt_csvs_folder + '/ka_train.csv',
     'dev_csv': gt_csvs_folder + '/ka_dev.csv',
+    'splits': [0.9, 0.1]
+}
+
+#Kaggle's dataset (ran through pyroom, 4 diff. locs.)
+KAx4 = {
+    'dataset_ID': 'KAx4',
+    'use_dataset': 0,
+    'dict': data_root + '/dict/ka_dict.pickle',
+    'transcript': data_root + '/spctrgrms/pyroom/KAx4/transcript.txt',
+    'train_csv': gt_csvs_folder + '/ka_x4_train.csv',
+    'dev_csv': gt_csvs_folder + '/ka_x4_dev.csv',
     'splits': [0.9, 0.1]
 }
 
@@ -182,39 +202,35 @@ other_dicts = []
 
 #Specify which datasets you want to use for training
 datasets = [TS_data, KA_data, TI_train, TI_test, SC_data, AO_engl, AO_span,
-            TS_spang, TS_kwords]
+            TS_spang, TS_kwords, TSx4, KAx4]
 #Location of "final" csvs, the ones that will be used to train and validate
 train_csv = gt_csvs_folder + '/all_train.csv'
 dev_csv = gt_csvs_folder + '/all_dev.csv'
 
-#FIND_LR-----------------------------------------------------------------------
-start_lr = 3e-5
-max_lr = 1.0
-
 #TRAIN------------------------------------------------------------------------
 other_chars = [' '] # other_chars = ["'", ' ']
 manual_chars = ['!','?','(',')','+','*','#','$','&','-','=',':']
-early_stop = {'n': 8, 'p': 0.999, 't': 1.0, 'w': 8}
+early_stop = {'n': 4, 'p': 0.999, 't': 1.0, 'w': 8}
 #TM Will be multiplied by the 'time' length of the spectrograms
 FM, TM = 27, 0.125 #Frequency and Time Masking Attributes
+specAug = False #Whether to use spec augment during training
 
 #Indicate which elements to freeze and which ones not to. If set to False, I
 #will freeze. If set to True, I won't. Last layer will be edited either way.
-CNN = {'change': False, 'cnn1_filters': 80, 'cnn1_kernel': 15,
+CNN = {'change': False, 'cnn1_filters': 4, 'cnn1_kernel': 3,
        'cnn1_stride': cnstnt.CNN_STRIDE}
-GRU = {'change': False, 'gru_dim': 16, 'gru_hid_dim': 16, 'gru_layers': 2,
+GRU = {'change': False, 'gru_dim': 32, 'gru_hid_dim': 32, 'gru_layers': 2,
        'gru_dropout': 0.1}
 #As of right now, number of mels must remain the same; n_mels = 128
 
 #Other hyper parameters
 HP = {'n_class': {'change': True, 'value': -1}, #dynamically edited later
-      'dropout': {'change': True, 'value': 0.1}, #classifier's dropout
-      'lr':      {'change': True, 'value': 0.005}, #learning rate
+      'dropout': {'change': False, 'value': 0.1}, #classifier's dropout
+      'e_0':      {'change': True, 'value': 3e-2}, #learning rate
+      'T':      {'change': True, 'value': -1}, #learning rate's coeff.
       'bs':      {'change': False, 'value': 2}, #batch size
       'epochs':  {'change': True, 'value': 10}}
 
-gamma = 0.95 #for learning scheduler
-#TODO update numner of classes later!
 #YOU SHOULDN'T HAVE TO EDIT ANY VARIABLES FROM HERE ON
 ##############################################################################
 #Make sure that assumed-path-to-desktop exists
@@ -238,10 +254,11 @@ ipa2char, char2ipa, int2char, char2int, blank_label = get_mappings(datasets,
     
 # PREPROCESSING --------------------------------------------------------------
 #In a nutchell: check audios and create csvs for training
+random.seed(7)
 preprocess_data(gt_csvs_folder, k_words, datasets, train_csv, dev_csv,
                 k_words_path, misc_log)
     
-if TRAIN or FIND_LR: #--------------------------------------------------------   
+if TRAIN: #--------------------------------------------------------   
     HP['n_class']['value'] = blank_label+1 #Initialize new number of classes (labels) 
     msg = "Training and Validation results are saved here:\n\n"
     log_message(msg, train_log, 'w', False)
@@ -257,9 +274,8 @@ if TRAIN or FIND_LR: #--------------------------------------------------------
     train_dataset = CUSTOM_DATASET(train_csv, ipa2char)
     dev_dataset = CUSTOM_DATASET(dev_csv, ipa2char)
     
-    #Initialize seeds
+    #Initialize torch's seed
     torch.manual_seed(7)
-    random.seed(7)
     
     #Load checkpoint and its hyper parameters
     checkpnt = torch.load(prev_chckpt_path)
@@ -280,18 +296,18 @@ if TRAIN or FIND_LR: #--------------------------------------------------------
     model, hparams, oparams = update_bigru(CNN, GRU, model, hparams, oparams)
     model, oparams = update_classifier(GRU, model, hparams, oparams)
     model = model.to(device)
-    optimizer = optim.AdamW(oparams, hparams['lr'])
+    optimizer = optim.AdamW(oparams, hparams['e_0'])
         
     #Initialize loss function (criterion) and learning rate scheduler
     criterion = nn.CTCLoss(blank=blank_label).to(device)
-    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma = gamma)
+    scheduler = LR_SCHED(hparams)
     
     #Set up Train DataLoader and Validation DataLoader    
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = data.DataLoader(dataset=train_dataset,
                                     batch_size=hparams['bs'],
                                     shuffle=True,
-                                    collate_fn=lambda x: data_processing(x, char2int, FM, TM, 'train'),
+                                    collate_fn=lambda x: data_processing(x, char2int, FM, TM, specAug),
                                     **kwargs)
     dev_loader = data.DataLoader(dataset=dev_dataset,
                                 batch_size=hparams['bs'],
@@ -299,83 +315,78 @@ if TRAIN or FIND_LR: #--------------------------------------------------------
                                 collate_fn=lambda x: data_processing(x, char2int),
                                 **kwargs)
         
-    if FIND_LR:
-        find_best_lr(model, criterion, optimizer, train_loader, start_lr,
-            max_lr, device)
-        
-    if TRAIN:
-        #To keep track of run time, PERs and best model
-        pers, best_per, best_model_wts, epoch_num = [], 2.0, {}, ''
-        start_time = time.time()
-        
-        metrics = Metrics()
-        MSG = '\t'
-        
-        for epoch in range(1, hparams['epochs'] + 1):
-            train(model, device, train_loader, criterion, optimizer,
-                scheduler, epoch, train_log, blank_label, int2char, char2ipa, 
-                metrics)
-            
-            dev(model, device, dev_loader, criterion, epoch,
-                train_log, blank_label, int2char, char2ipa, metrics)
-            
-            #If current PER is lower than best global PER, copy the model
-            epoch_per = metrics.dev_pers[-1]
-            if epoch_per < best_per:
-                best_per = epoch_per
-                best_model_wts = copy.deepcopy(model.state_dict())
-                # optimizer_state_dict = copy.deepcopy(optimizer.state_dict())
-                epoch_num = str(epoch)
-            
-            stop, stop_msg = metrics.should_we_stop(epoch, early_stop)
-            if stop: #Early Stop
-                MSG += stop_msg
-                break
-        
-        msg = MSG + f"Best PER: {metrics.get_best_cer():.4f} on Epoch "
-        msg += f"{metrics.dev_pers.index(metrics.get_best_cer()) + 1}\n"
-        log_message(msg + '\n\n', train_log, 'a', True)
-        
-        #Log model summary, # of parameters, hyper parameters and more
-        num_params = log_model_information(misc_log, model, hparams)
-            
-        #Plot losses, PERs, learning rates and save as figures
-        plot_and_save(metrics.dev_losses, metrics.train_losses, metrics.dev_pers,
-                metrics.train_pers, metrics.lrs, 1, logs_folder)
-            
-        #Save weights of best model, along with its optimizer state and hparams
-        new_chckpt_path = save_chckpnt(best_model_wts, hparams, new_chckpt_path,
-            '1', epoch_num)
-        
-        #Log summary of values, paths and attributes used
-        msg += f"Checkpoint has been saved here: {new_chckpt_path}\n"
-        msg += f"Was the CNN changed? {CNN['change']}\n"
-        msg += f"Was the GRU changed? {GRU['change']}\n"
-        msg += f"Number of parameters in model: {num_params}\n"
-        msg += "Are we using masking during training? 'Yes'\n"
-        msg += f"Training set had {len(train_dataset)} audio files "
-        msg += f"equivalent to {train_dataset.duration:.2f} seconds\n"
-        msg += f"In all runs, dev set had {len(dev_dataset)} audio files; equi"
-        msg += f"valent to {dev_dataset.duration:.2f} seconds\n"
-        msg += f"Early Stop Values:\n\tn: {early_stop['n']}\n\tPercentage: "
-        msg += f"{((1-early_stop['p'])*100):.2f}%\n\tOverfit Threshold: "
-        msg += f"{early_stop['t']:.2f}\n\tNumber of epochs to wait: "
-        msg += f"{early_stop['w']}\n"
-        msg += f"Gamma value for learning rate is: {gamma}\n"
-        msg += f"Number of classes: {hparams['n_class']}\n"
-        msg += f"Time Masking Coefficient: {TM}, Frequency Masking: {FM}\n"
-        msg += f"This run took {(time.time() - start_time):.2f} seconds\n"
-        log_message(msg, train_log, 'a', True)
-        
-        #Log labels' conversions (from IPA to char and char to int)
-        log_labels(ipa2char, char2int, misc_log)
-        #Log & print the number of times each k_word appears in train and dev
-        log_k_words_instances(k_words_path, misc_log)
+    #To keep track of run time, PERs and best model
+    pers, best_per, best_model_wts, epoch_num = [], 2.0, {}, ''
+    start_time = time.time()
     
-        print("\nModels summaries, hyper parameters and other miscellaneous info "
-              f"can be found here: {misc_log}")
-        print(f"A log for all trainings can be found here: {train_log}")
-        print(f"Losses' Plots for each run can be found here: {logs_folder}")
+    metrics = Metrics()
+    MSG = '\t'
+    
+    for epoch in range(1, hparams['epochs'] + 1):
+        train(model, device, train_loader, criterion, optimizer,
+            scheduler, epoch, train_log, blank_label, int2char, char2ipa, 
+            metrics)
+        
+        dev(model, device, dev_loader, criterion, epoch,
+            train_log, blank_label, int2char, char2ipa, metrics)
+        
+        #If current PER is lower than best global PER, copy the model
+        epoch_per = metrics.dev_pers[-1]
+        if epoch_per < best_per:
+            best_per = epoch_per
+            best_model_wts = copy.deepcopy(model.state_dict())
+            # optimizer_state_dict = copy.deepcopy(optimizer.state_dict())
+            epoch_num = str(epoch)
+        
+        stop, stop_msg = metrics.should_we_stop(epoch, early_stop)
+        if stop: #Early Stop
+            MSG += stop_msg
+            break
+    
+    msg = MSG + f"Best PER: {metrics.get_best_cer():.4f} on Epoch "
+    msg += f"{metrics.dev_pers.index(metrics.get_best_cer()) + 1}\n"
+    log_message(msg + '\n\n', train_log, 'a', True)
+    
+    #Log model summary, # of parameters, hyper parameters and more
+    num_params = log_model_information(misc_log, model, hparams)
+        
+    #Plot losses, PERs, learning rates and save as figures
+    plot_and_save(metrics.dev_losses, metrics.train_losses, metrics.dev_pers,
+            metrics.train_pers, metrics.lrs, 1, logs_folder)
+        
+    #Save weights of best model, along with its optimizer state and hparams
+    new_chckpt_path = save_chckpnt(best_model_wts, hparams, new_chckpt_path,
+        '1', epoch_num)
+    
+    #Log summary of values, paths and attributes used
+    msg += f"Checkpoint has been saved here: {new_chckpt_path}\n"
+    msg += f"Was the CNN changed? {CNN['change']}\n"
+    msg += f"Was the GRU changed? {GRU['change']}\n"
+    msg += f"Number of parameters in model: {num_params}\n"
+    msg += f"Training set had {len(train_dataset)} audio files "
+    msg += f"equivalent to {train_dataset.duration:.2f} seconds\n"
+    msg += f"Dev set had {len(dev_dataset)} audio files; equivalent"
+    msg += f" to {dev_dataset.duration:.2f} seconds\n"
+    msg += f"Early Stop Values:\n\tn: {early_stop['n']}\n\tPercentage: "
+    msg += f"{((1-early_stop['p'])*100):.2f}%\n\tOverfit Threshold: "
+    msg += f"{early_stop['t']:.2f}\n\tNumber of epochs to wait: "
+    msg += f"{early_stop['w']}\n"
+    msg += f"Number of classes: {hparams['n_class']}\n"
+    msg += f"Are we using masking during training? {specAug}\n"
+    if specAug:
+        msg += f"Time Masking Coeff.: {TM}, Frequency Masking: {FM}\n"
+    msg += f"This run took {(time.time() - start_time):.2f} seconds\n"
+    log_message(msg, train_log, 'a', True)
+    
+    #Log labels' conversions (from IPA to char and char to int)
+    log_labels(ipa2char, char2int, misc_log)
+    #Log & print the number of times each k_word appears in train and dev
+    log_k_words_instances(k_words_path, misc_log)
+
+    print("\nModels summaries, hyper parameters and other miscellaneous info "
+          f"can be found here: {misc_log}")
+    print(f"A log for all trainings can be found here: {train_log}")
+    print(f"Losses' Plots for each run can be found here: {logs_folder}")
     
     #Delete model, collect garbage and empty CUDA memory
     #See: https://stackify.com/python-garbage-collection/
